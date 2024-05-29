b2a4a2f8f15cb59b341b2a7c26b995d6d582e02e
ajipon-44 <63995620+ajipon-44@users.noreply.github.com>
2024-05-29 09:30:31 +0900

    commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, watch } from 'vue';
+const todo = ref('');
+const todos = ref([]);
+const createTodo = () => {
+  todos.value.push(todo.value);
+  todo.value = '';
+};
+</script>
+<template>
+  <div>
+    <input v-model="todo" type="text" />
+    <button @click="createTodo">create todo</button>
+    <ul>
+      <!-- todos 配列の内容を表示 -->
+      <li v-for="(item, index) in todos" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
